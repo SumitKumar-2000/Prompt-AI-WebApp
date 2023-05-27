e5d6d147552dd664097107f3f6212cf44f096e6b
SumitKumar-2000 <sumitkumarracer@gmail.com>
2023-05-27 17:04:14 +0530

    [Feed.jsx] searching functionality added using regex

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react"
 import PromptCard from "./PromptCard"
 
-const PromCardList = ({data, handleTagClick}) => {
+const PromptCardList = ({data}) => {
   return (
     <div className="mt-16 prompt_layout">
       {
@@ -21,9 +21,12 @@ const PromCardList = ({data, handleTagClick}) => {
 
 const Feed = () => {
 
-  const [searchText, setSearchText] = useState("");
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+  
   useEffect(() => {
     (async () => {
       const response = await fetch("/api/prompt");
@@ -32,22 +35,51 @@ const Feed = () => {
     })()
   },[])
 
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    console.log("search timeout: ",searchTimeout);
+    setSearchQuery(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
   return (
     <section className="feed">
-      <form className="w-full relative flex-center">
+      <form 
+        className="w-full relative flex-center"
+      >
         <input 
           type="search" 
           placeholder="Search for a tag or a username"
-          value={searchText}
-          onChange={e=>setSearchText(e.target.value)}
+          value={searchQuery}
+          onChange={handleSearchChange}
           className="search_input"
         />
       </form>
 
-      <PromCardList
-        data={posts}
-        handleTagClick={() => {}}
-      />
+       {searchQuery ? (
+        <PromptCardList
+          data={searchedResults}
+        />
+      ) : (
+        <PromptCardList data={posts}/>
+      )}
     </section>
   )
 }
